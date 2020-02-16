9980dbc6275435c5acae7297dae4f3fc22edde22
Viktor Vasilyrv <Kobe001@mail.ru>
2020-02-16 14:54:33 +0400

    Merge remote-tracking branch 'origin/master' 3.  А существует ли треугольник # Conflicts: #	README.md

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -2,10 +2,8 @@ package ru.job4j.condition;
 
 public class Triangle {
     public static boolean exist(double ab,double ac,double bc){
-        boolean result = ab+ac>bc;
-        if (ac+bc>ab){
-            result = true;
-        }else if (ab+bc>ac){
+        boolean result = ab + ac > bc;
+        if (ac + bc > ab && ab + bc > ac){
             result = true;
         }
         return false;
